58d587f7a6f5b3695c24822406c57f724e9feb33
valentino <valentino.digiosaffatte@student.univaq.it>
2020-05-12 21:34:12 +0200

    card component proto

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,12 @@
 import React, {useEffect} from 'react';
-import {SafeAreaView, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import {sLoadedTvShow} from '../reducers/AppReducer';
 import {useSelector, useDispatch} from 'react-redux';
 import {fetchTvShowDetails} from '../actions';
@@ -10,25 +17,54 @@ export default function() {
 
   useEffect(() => {
     dispatch(fetchTvShowDetails());
-  }, []);
+  }, [dispatch]);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <Text>{tvshow.first_air_date}</Text>
+      <View style={styles.card_container}>
+        <TouchableOpacity style={styles.card}>
+          <Image
+            style={styles.card_image}
+            source={{uri: 'https://reactjs.org/logo-og.png'}}
+          />
+          <View style={styles.card_text_container}>
+            <Text numberOfLines={2} style={styles.card_text}>
+              hello world string string string str string
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     flex: 1,
-    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
   },
-  text: {
-    color: 'purple',
-    fontSize: 40,
+  card_container: {
+    width: '33,3333%',
+    padding: 10,
+  },
+  card: {
+    backgroundColor: '#888888',
+    padding: 3,
+    borderRadius: 5,
+  },
+  card_image: {
+    width: '100%',
+    height: 150,
+    borderRadius: 5,
+  },
+  card_text_container: {
+    padding: 3,
+  },
+  card_text: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 15,
     fontWeight: 'bold',
   },
 });
